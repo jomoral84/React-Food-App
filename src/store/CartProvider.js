4d9e53eb37cf8704ff766ee784215236caf3cca4
your name <your@email.com>
2022-01-10 16:22:42 -0300

    Se completa el CartProvider.js

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,47 @@
+import { useReducer } from 'react';
+
 import React from 'react';
 import CardContext from './cart-context';
 
+
+const defaultCartState = {
+   items: [],
+   totalAmount: 0
+}
+
+
+ const cartReducer = (state, action) => {
+if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+       items: updatedItems,
+       amount: updatedTotalAmount
+    }
+}
+
+    return defaultCartState;
+ }
+
+
 const CartProvider = (props) => {
+   
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-   const addItemHandler = (item) => {};
 
-  const removeItemHandler = (id) => {};
+   const addItemHandler = (item) => { 
+      dispatchCartAction({type: 'ADD', item: item})
+   };
+
+  const removeItemHandler = (id) => {
+   dispatchCartAction({type: 'REMOVE', id: id})
+  };
    
 
 
    const cartContext = {
-      items: [],
-      totalAmount: 0,
+      items: cartState.items,
+      totalAmount: cartState.totalAmount,
       addItem: addItemHandler,
       removeItem: removeItemHandler,
 
