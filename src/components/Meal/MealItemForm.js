b7005367112e88edb94a17d9f8de30515ec0ce71
your name <your@email.com>
2022-01-11 14:23:09 -0300

    Se agrego la validacion de la cantidad

--- src/components/Meal/MealItemForm.js
+++ src/components/Meal/MealItemForm.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React , { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../UI/Input';
 
 
 const MealItemForm = (props) => {
+
+     const [amountisValid, setAmountIsValid] = useState(true);
+      
+
+    const amountInputRef = useRef();
+   
+ 
+    const submitHandler = (event) => {
+        event.preventDefault(); 
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;     // Convierte el type "number" String a un numero entero
+    
+      if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5)  {     // Validacion del formulario
+        setAmountIsValid(false);  
+        return;
+      }
+   
+       props.onAddToCart(enteredAmountNumber);
+   
+    }
+
+
+
+
     return (
-       <form className={classes.form}>
-          <Input label="Cantidad" input={{
-              id: 'cantidad_' + props.id,
+       <form className={classes.form} onSubmit={submitHandler}>
+          <Input
+              ref={amountInputRef} 
+              label="Cantidad" input={{
+              id: 'amount_' + props.id,
               type: 'number',
               min: '1',
               max: '5',
@@ -17,6 +44,7 @@ const MealItemForm = (props) => {
           }}/>
         
         <button>+ Añadir</button>
+        {!amountisValid && <p>Ingrese una cantidad valida (1-5)</p>}
 
 
        </form>
